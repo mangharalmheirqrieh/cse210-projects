071e0e7d2e086047da397b75abccd1a2f4feafe1
mangharalmheirqrieh <tennoplayer123@gmail.com>
2024-03-05 00:27:34 +0800

    complete Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,3 +1,6 @@
+// Mheirqrieh F. Mangharal
+// March 4 , 2024
+
 using System;
 
 class Program
@@ -5,5 +8,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+        int attemptCount;
+
+        string response;
+        
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,11);
+            int guess = -1;
+            attemptCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write(" What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attemptCount++;
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }  
+            Console.WriteLine($"It took you {attemptCount} attemps");
+            
+            Console.Write("Do you want to continue? yes/no ");
+            response = Console.ReadLine();
+        }while (response == "yes");
     }
 }
