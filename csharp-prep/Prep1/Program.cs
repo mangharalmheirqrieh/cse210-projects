42c9b7805d0da9d4379fe6111790e8acacdd730d
mangharalmheirqrieh <tennoplayer123@gmail.com>
2024-03-04 21:40:28 +0800

    completed Learning Activity - C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,3 +1,6 @@
+// Mheirqrieh F. Mangharal
+// March 4 , 2024
+
 using System;
 
 class Program
@@ -5,5 +8,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine(" ");
+
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
